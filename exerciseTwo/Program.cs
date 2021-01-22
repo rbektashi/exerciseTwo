e427487e9783550e3cbe063659aaf28720e4c577
rbektashi <raeannamichele@gmail.com>
2021-01-22 17:17:12 -0500

    Initial check-in of module exerciseTwo

---
+++ exerciseTwo/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace exerciseTwo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Update your variable to ask the user for their social security number, store their input into a string variable
+            string greetingMessage = "Please enter your SSN";
+            Console.WriteLine(greetingMessage);
+            string SSN = Console.ReadLine();
+            // Display only the first digits of their SSN
+            Console.WriteLine(SSN.Remove(3,8));
+            // Display only the last four digits of their SSN
+            Console.WriteLine(SSN.Remove(0, 7));
+            // Display the whole SSN removing all dashes and replacing all but the last four digits with the * symbol
+            Console.WriteLine("*****" + SSN.Substring(SSN.Length - 4, 4));
+
+        }
+    }
+}
